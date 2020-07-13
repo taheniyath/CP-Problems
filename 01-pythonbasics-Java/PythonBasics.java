f9d6937241ffac2b165565821564c5362d70c6dc
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-13 17:54:26 +0530

    Jul 13  17:54:26

--- 01-pythonbasics-Java/PythonBasics.java
+++ 01-pythonbasics-Java/PythonBasics.java
@@ -1,3 +1,5 @@
+import jdk.internal.vm.PostVMInitHook;
+
 // Write a non static method called "show_excitement" where the string
 // "I am super excited for this course!" is returned exactly
 // 5 times, where each sentence is separated by a single space.
@@ -7,7 +9,16 @@
 
 
 public class PythonBasics {
+	String s;
     public String show_excitement() {
-    	// your code goes here
+		// your code goes here
+		for(int i=0; i<5; i++){
+			 s = s + "I am super excited for this course! ";
+		}
+		return s;
+	}
+	public static void main(String[] args) {
+		PythonBasics s = new PythonBasics();
+		System.out.println(s.show_excitement());
 	}
 }
