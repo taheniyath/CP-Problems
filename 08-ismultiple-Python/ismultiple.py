76140dfe5a1429b59921c2036e756044e3deff8d
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-14 00:19:34 +0530

    Jul 14  00:19:33

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -6,4 +6,13 @@
 
 
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if(n==0 and m==0):
+		return True
+	elif(n==0):
+		return False
+	elif(m==0):
+		return True
+	elif(m%n==0):
+		return True
+	else:
+		return False # replace with your solution
