8cd256ef3ed2ad7355c4e0563af4f6bafdff5790
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-13 23:26:53 +0530

    Jul 13  23:26:53

--- 04-circlesintersect-Python/circlesintersect.py
+++ 04-circlesintersect-Python/circlesintersect.py
@@ -6,4 +6,10 @@
 
 def fun_circlesintersect(x1, y1, r1, x2, y2, r2):
 	# your code goes here
-	return False 
+	dist1 = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+	dist2 = (r1+r2)*(r1+r2);
+	if(dist1<dist2):
+		return True	
+	else:
+		return False
+	 
