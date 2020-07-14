f531628176ad8038ca8ec1271789a44e1be8dbb1
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-14 08:50:56 +0530

    Jul 14  08:50:56

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -6,4 +6,12 @@
 
 def fun_eggcartons(eggs):
 	# your code goes here
+	if(eggs==0):
+		return 0
+	elif(eggs<=12):
+		return 1
+	elif(eggs>12 and eggs<=24):
+		return 2
+	else:
+		return 3
 	return 1
