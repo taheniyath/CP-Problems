c115c21ab18a134a8890fe893955d08a3f7452e0
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-14 11:22:54 +0530

    Jul 14  11:22:53

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -4,7 +4,17 @@
 # assert(handToDice(214) == (2,1,4))
 # assert(handToDice(422) == (4,2,2))
 # Hint: You might find // and % useful here, and also getKthDigit().
-
+def getkthDigit(hand, n):
+	num = abs(hand)
+	res1 = num%10^(n+1)
+	res2 = num%10^(n)
+	dig = (res2-res1)/10^(n)
+	return dig
 def handtodice(hand):
 	# your code goes here
-	pass
+	k1 = getkthDigit(hand,0)
+	k2 = getkthDigit(hand,1)
+	k3 = getkthDigit(hand,2)
+	m = (k1,k2,k3)
+	return m
+	# pass
