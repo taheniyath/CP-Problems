9381cf15c4593df4d6294a3e8fbf48803036d9e3
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-14 19:04:39 +0530

    Jul 14  19:04:39

--- 02-pythonpractice-Python/pythonpractice.py
+++ 02-pythonpractice-Python/pythonpractice.py
@@ -18,3 +18,20 @@ Use the test cases below to guide you!"""
 class Classy(object):
     def __init__(self):
         self.items = []
+        self.c = 0
+
+        
+    def classiness(self):
+        return self.c
+
+
+    def addItem(self, s):
+        if s == "tophat":
+            self.c += 2
+        elif s== "bowtie":
+            self.c += 4
+        elif s == "monocle":
+            self.c += 5
+        else :
+            self.c += 0
+        return self.c
