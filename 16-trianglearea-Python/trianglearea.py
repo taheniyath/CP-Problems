7205cacc221a242d754cf3e674ab1f58c035a05d
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-14 17:45:43 +0530

    Jul 14  17:45:43

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -1,3 +1,4 @@
+import math
 # trianglearea(s1, s2, s3)[5pts]
 # Write the function trianglearea(s1, s2, s3) that takes 3 floats/ints and returns the area of the
 # triangle that has those lengths of its side. If no such triangle exists, return 0. Hint: you
