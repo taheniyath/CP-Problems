a8b0da86fa1634f0e1d37ffa28313b20f488c396
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-14 17:13:28 +0530

    Jul 14  17:13:27

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -1,3 +1,4 @@
+import math
 # isrighttriangle(x1, y1, x2, y2, x3, y3)[5pts]
 # Write the function isrighttriangle(x1, y1, x2, y2, x3, y3) that takes 6 int or float values that
 # represent the vertices (x1,y1), (x2,y2), and (x3,y3) of a triangle, and returns True if that is
@@ -7,4 +8,15 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	x = math.sqrt((x2-x1)**(2) +(y2-y1)**(2))
+	y = math.sqrt((x3-x2)**(2) +(y3-y2)**(2))
+	z = math.sqrt((x1-y3)**(2) +(y1-y3)**(2))
+	x1 = x**(2)
+	y1 = y**(2)
+	z1 = z**(2)
+	if(math.isclose(x1,(y1+z1)) or math.isclose(y1,(x1+z1)) or math.isclose(z1,(y1+x1)) ):
+		return True
+	else:
+		return False
+
+	# pass
