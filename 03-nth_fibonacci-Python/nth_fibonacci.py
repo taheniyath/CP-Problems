6caae696aa89f1cd5ccc1f5dd932e922d4281d19
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-16 09:20:12 +0530

    Jul 16  09:20:12

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -9,9 +9,11 @@
 
 def fun_nthfibonaccinumber(n):
 	# your code goes Here
+	k = 0
 	if(n==0):
 		return 1
 	if(n>=1):
-		k = (n-1) + (n-2)
+		for i in range(n):
+			k = k+((i-1) + (i-2))
 		return k
 	return 0
