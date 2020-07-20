3763bcbc5163acb267af113c24f197cb38b2f17c
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-20 14:49:58 +0530

    Jul 20  14:49:58

--- 07-removeduplicate-Python/removeduplicate.py
+++ 07-removeduplicate-Python/removeduplicate.py
@@ -3,6 +3,8 @@
 # if given String is "JavaPython" then the output should be "JavPython".
 # The second or further occurrence of duplicate should be removed.
 
+from collections import OrderedDict
 def removeduplicate(text):
 	# Your code goes here
+	return "".join(OrderedDict.fromkeys(text))
 	pass
