df48fa2b6d28c5d2bf96a4fe27a4fa5c441b495c
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-20 18:11:07 +0530

    Jul 20  18:11:07

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -11,12 +11,9 @@ def isrotated(str1, str2):
 	if (x != y):
 		return 0
 	temp = str1 + str2
-	i = 0
-	if temp[i:i+y] == str2:
-
+	if str2 in temp:
 		return True
 	else:
-		i = i+1
 		return False
 
 	# pass
