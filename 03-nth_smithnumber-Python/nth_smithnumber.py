96e83f39cffd98425fbcb164419dabfc841bb40b
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-25 15:32:42 +0530

    Jul 25  15:32:42

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -10,7 +10,7 @@
 def isprime(n):
     if(n>1):
         for i in range(2,n):
-            if(n%i == 0):
+            if((n%i) == 0):
                 return False
             else:
                 return True
