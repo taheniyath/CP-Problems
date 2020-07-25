a65e7958b6a6c3b7fecdc1e30f46db5f40db37c8
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-25 07:13:07 +0530

    Jul 25  07:13:07

--- 10-destructiveshortenlongruns-Python/destructiveshortenlongruns.py
+++ 10-destructiveshortenlongruns-Python/destructiveshortenlongruns.py
@@ -1,4 +1,4 @@
-# Here we will rewrite the previous function to be destructive. This version must not just call the nondestructive 
+# Here we will rewrite the previous function to ye destructive. This version must not just call the nondestructive 
 # version from above and then clear and replace the values in the original list. Instead, you must traverse the list 
 # once and make the changes to the list as you go. With that in mind, write the destructive function shortenLongRuns(
 # L, k) that takes a list L and a positive integer k, and destructively modifies L by removing any values in L that 
@@ -19,16 +19,16 @@ def destructiveshortenlongruns(L, k):
 	z=0
 	while x<len(L):
 		if n==L[x]:
-			b=b+1
-			if b>=k:
+			y=y+1
+			if y>=k:
 				L.pop(x)
 				x-=1
 		elif x==0:
 			n = L[x]
-			b = b+1
+			y = y+1
 		else:
 			n = L[x]
-			b=1
+			y=1
 		x=x+1
 	return L
 	# pass
