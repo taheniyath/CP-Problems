7742665bdfd9d9ada32303d1a93c8aff07f4a52e
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-25 16:47:04 +0530

    Jul 25  16:47:04

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -4,7 +4,23 @@
 # fun_carrylessadd(x, y) that takes two non-negative integers x and y and returns their 
 # carryless sum. As the paper demonstrates, fun_carrylessadd(785, 376) returns 51.
 
-
+import math
 def fun_carrylessadd(x, y):
-	return 0
+	sum = 0
+	if(x==0):
+		return y
+	if(y == 0):
+		return x
+	n = 1
+	sum1 = 0
+	while(x or y):
+		sum1 = ((x%10 +y%10))
+		sum1 = sum1%10
+		sum = (sum1*n) + sum
+		x = math.floor(x/10)
+		y = math.floor(y/10)
+		m = m*10
+	s = str(sum)
+	s.strip("0")
+	return int(s)
 
