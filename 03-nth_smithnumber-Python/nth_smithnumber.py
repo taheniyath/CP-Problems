124f96d79faffeb25ee4d84702f37ea0b1c6c03b
Taheniyath-khan <syednisar001@msitprogram.net>
2020-07-25 15:47:59 +0530

    Jul 25  15:47:58

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -14,6 +14,31 @@ def isprime(n):
                 return False
             else:
                 return True
+def sumofdig(n):
+    x = 0
+    while n>0:
+        x = x + n%10
+        n = n//10
+    return x
+
+def sumoffact(n):
+    l =[]
+    while n%2 == 0 and n>0:
+        l.append(int(2))
+        n = n/2
+    for i in range(3, int(math.sqrt(n))+1,2):
+        while n%i == 0:
+            l.append(int(i))
+            n = n/1
+    if n>2:
+        l.append(int(n))
+    s = 0
+    for l in l:
+        if len(str((i))) == 1:
+            s = s + i
+        elif len(str(i) > 1 and 1 is not n):
+            s = s + sumofdig(i)
+        return s
 def fun_nth_smithnumber(n):
 
 
